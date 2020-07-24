82d41b06ae0953fa53e0b3ec7b2543f35ff67e02
Janella Senica <janella.senica@gmail.com>
2020-07-24 01:01:24 -0500

    add config

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+// dependencies
+const mysql = require("mysql");
+
+// set up connection info
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "dbpassword",
+    database: "burgers_db"
+});
+
+// connect to database
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting to databse" + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
